a74e5ae70227b8cb5ff312ba6fd165bd6b1108c9
michealcoleyoung <coleyoungmusic@gmail.com>
2022-09-03 12:20:00 -0500

    Updated TODO list

--- main.py
+++ main.py
@@ -12,12 +12,11 @@ if __name__ == "__main__":
                         datefmt='%Y-%m-%d %H:%M:%S')
 
     # TODO: ask user to specify a few files they would like to monitor
-    # TODO: ask user to specify a few files they would like to monitor
     # TODO: retrieve filepath and store via a dictionary
     # TODO: output any modifications made to these paths in the terminal
 
     # set path
-    path = ""
+    path = "/Users/michealyoung/Desktop/test-change/test-spreadsheet.ods"
 
     # intiliaze logging event handler
     event_handler = LoggingEventHandler()
