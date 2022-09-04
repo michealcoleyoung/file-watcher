5304a9475037ecef4ad867b5232c52766e732b6e
michealcoleyoung <coleyoungmusic@gmail.com>
2022-09-04 15:39:29 -0500

    Updated instructions on TODOs and added additional logic for later

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import logging
 import watchdog.events
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
+from tkinter import filedialog as fd
 
 
 if __name__ == "__main__":
@@ -11,19 +12,26 @@ if __name__ == "__main__":
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
 
-    # TODO: ask user to specify a few files they would like to monitor
+    # TODO: ask user to specify a few files they would like to watch from top to bottom as read from text file
     # TODO: retrieve filepath and store via a dictionary
-    # TODO: output any modifications made to these paths in the terminal
+    # TODO: output any modifications made to these files in the terminal
 
     # set path
-    path = "/Users/michealyoung/Desktop/test-change/test-spreadsheet.ods"
+    # user_input = input("Enter the name of the files you would like to watch: ")
+    # paths = user_input.split()
+    # print(paths)
+
+    with open('/Users/michealyoung/Downloads/test-file-list.txt') as f:
+        print(f.readlines())
 
     # intiliaze logging event handler
     event_handler = LoggingEventHandler()
 
     # initialize observer
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
+
+    for path in paths:
+        observer.schedule(event_handler, path, recursive=True)
 
     # start observer
     observer.start()
