3ab8280830a299c4c873fea98add72af5df9e1e3
michealcoleyoung <coleyoungmusic@gmail.com>
2022-09-05 18:04:53 -0500

    Added docstring with instructions

--- main.py
+++ main.py
@@ -7,6 +7,34 @@ from watchdog.events import LoggingEventHandler
 from tkinter import filedialog as fd
 
 
+def user_instructions():
+    """
+ /$$$$$$$$ /$$ /$$                 /$$      /$$             /$$               /$$
+| $$_____/|__/| $$                | $$  /$ | $$            | $$              | $$
+| $$       /$$| $$  /$$$$$$       | $$ /$$$| $$  /$$$$$$  /$$$$$$    /$$$$$$$| $$$$$$$   /$$$$$$   /$$$$$$
+| $$$$$   | $$| $$ /$$__  $$      | $$/$$ $$ $$ |____  $$|_  $$_/   /$$_____/| $$__  $$ /$$__  $$ /$$__  $$
+| $$__/   | $$| $$| $$$$$$$$      | $$$$_  $$$$  /$$$$$$$  | $$    | $$      | $$  \ $$| $$$$$$$$| $$  \__/
+| $$      | $$| $$| $$_____/      | $$$/ \  $$$ /$$__  $$  | $$ /$$| $$      | $$  | $$| $$_____/| $$
+| $$      | $$| $$|  $$$$$$$      | $$/   \  $$|  $$$$$$$  |  $$$$/|  $$$$$$$| $$  | $$|  $$$$$$$| $$
+|__/      |__/|__/ \_______/      |__/     \__/ \_______/   \___/   \_______/|__/  |__/ \_______/|__/
+
+Instructions:
+1. Create a plain text file with a list of filepaths that you would like to monitor. Make sure the path is exact.
+
+EXAMPLE:
+C:/Documents/Newsletters/Summer2018.pdf
+C:/Documents/Newsletters/Summer2019.pdf
+C:/Documents/Newsletters/Summer2020.pdf
+C:/Documents/Newsletters/Summer2021.pdf
+
+2. As changes are made you will see updates to these files in realtime
+
+    """
+
+
+print(user_instructions.__doc__)
+
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
@@ -16,28 +44,30 @@ if __name__ == "__main__":
     # TODO: retrieve filepath and store via a dictionary
     # TODO: output any modifications made to these files in the terminal
 
+    input('Press enter to continue...')
+
     # set path
-    # user_input = input("Enter the name of the files you would like to watch: ")
-    # paths = user_input.split()
-    # print(paths)
+    path = fd.askopenfilename()
+    print("*** Watching Files ***")
 
-    with open('/Users/michealyoung/Downloads/test-file-list.txt') as f:
-        print(f.readlines())
+    # reads from txt file with list of
+    with open(path) as f:
+        new_list = [x.strip('\n') for x in f]
 
-    # intiliaze logging event handler
-    event_handler = LoggingEventHandler()
+        # intiliaze logging event handler
+        event_handler = LoggingEventHandler()
 
-    # initialize observer
-    observer = Observer()
+        # initialize observer
+        observer = Observer()
 
-    for path in paths:
-        observer.schedule(event_handler, path, recursive=True)
+        for path in new_list:
+            observer.schedule(event_handler, path, recursive=True)
 
-    # start observer
-    observer.start()
-    try:
-        while True:
-            time.sleep(1)
-    finally:
-        observer.stop()
-        observer.join()
+        # start observer
+        observer.start()
+        try:
+            while True:
+                time.sleep(1)
+        finally:
+            observer.stop()
+            observer.join()
